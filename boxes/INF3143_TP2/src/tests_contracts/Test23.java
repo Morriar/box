7745d8aa3676274a116e0362d6f3a1b5f29148e8
Alexandre Terrasa <alexandre@moz-code.org>
2017-04-25 18:54:00 -0400

    boxes/INF3143_TP2: fix Test23
    Signed-off-by: Alexandre Terrasa <alexandre@moz-code.org>

--- boxes/INF3143_TP2/src/tests_contracts/Test23.java
+++ boxes/INF3143_TP2/src/tests_contracts/Test23.java
@@ -9,20 +9,10 @@ public class Test23 extends LightCombat {
 			Integer maxWeapons) {
 		super(id, volume, mass, hull, maxWeapons);
 	}
-	
-	@Override
-	public Boolean isDestroyed() {
-		return true;
-	}
-
-	@Override
-	public Integer getHull() {
-		return 0;
-	}
 
 	public static void main(String[] args) throws WeaponException {
 		Test23 t = new Test23("v1", 10, 20, 100, 2);
-		t.damage(50);
+		t.damage(0);
 	}
 
 }
